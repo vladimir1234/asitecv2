a8b4a76430abd413723ac9e57555d14b86ef6604
vsuncion <vladimir_sc@hotmail.com>
2022-11-03 06:56:02 -0500

    limpio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxBootstrapIconsModule,allIcons} from 'ngx-bootstrap-icons';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxBootstrapIconsModule.pick(allIcons)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
